249d35f3a6b648926017c21b6cf907056ed2bc60
紀博穎 <jiboying@MacBook-Pro.local>
2024-07-29 17:26:37 +0800

    0729-docs-create vite app

---
+++ fronted/src/App.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+import { reactive } from 'vue';
+
+let jwt = ''
+const data = reactive({});
+const getInfo = (()=>{
+  axios.get('/api/api/v1/user/rawdata/boin',{
+					headers:{
+						'Authorization' : jwt
+					}
+				})
+					.then(res =>{
+            Object.assign(data, res.data.data);
+					})
+					.catch(
+						console.log('axios 發生錯誤惹...')
+					)	
+})
+
+
+</script>
+
+<template>
+  <header>
+
+    <button @click="getInfo">按一下</button>
+    
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+</template>
+
+<style scoped>
+</style>
